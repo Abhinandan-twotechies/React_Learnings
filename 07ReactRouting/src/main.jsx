af412758b7574e0e19fe6099d2fc70706543c2eb
Abhinandan <abhinandan.twotechies@gmail.com>
2025-08-02 17:11:18 +0530

    added nested routing

--- 07ReactRouting/src/main.jsx
+++ 07ReactRouting/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider, Routes } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
@@ -10,14 +10,20 @@ import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element= {<Home/>} />
-      <Route path='about' element= {<About/>} />
-      <Route path='contact' element= {<Contact/>} />
-      <Route path='user/:id' element= {<User/>} />
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' >
+        <Route path='' element={<About/>}/>
+        <Route path='aboutUser' element = {<User/>}/>
+      </Route>
+      <Route path='contact' element={<Contact />} />
     </Route>
   )
 )
+
+{/* <Route path='' element={<About />} /> */}
+{/* <Route path='/about/aboutUser' element={<User />} /> */}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
