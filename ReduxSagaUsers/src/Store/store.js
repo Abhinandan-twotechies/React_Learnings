b36fd09b97b98b8c7b644ab7ca49ca9dc16a7056
Abhinandan <abhinandan.twotechies@gmail.com>
2025-08-21 18:21:03 +0530

    added changes

--- ReduxSagaUsers/src/Store/store.js
+++ ReduxSagaUsers/src/Store/store.js
@@ -9,7 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
 
 
